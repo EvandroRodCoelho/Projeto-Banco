3d686a798706697d107f3de83398cd622c2a2681
Fabricio-Lima <fabriciolc61@gmail.com>
2023-06-06 22:52:45 -0300

    feat: UsuarioModel
    Implementa model de Usuario

---
+++ App/src/model/Usuario.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Usuario {
+
+	private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private int acesso;
+
+    public Usuario(int Id, String Nome, String Email, String Senha, int Acesso){
+        this.id = Id;
+    	this.nome = Nome;
+        this.email = Email;
+        this.senha = Senha;
+        this.acesso = Acesso;
+    }
+
+    public int getId() {
+    	return id;
+    }
+    public String getnome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public int getAcesso() {
+        return acesso;
+    }
+}
+
