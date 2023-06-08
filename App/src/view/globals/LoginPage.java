0078a17327a901d64fc659bb8da9984207ed7a2e
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-06-07 22:01:04 -0300

    feat: :sparkles: User Register Feature

--- App/src/view/globals/LoginPage.java
+++ App/src/view/globals/LoginPage.java
@@ -11,12 +11,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.Hyperlink;
 import model.AppSession;
 import model.Usuario;
 import view.utils.ButtonComponent;
+import view.user.RegisterPage;
 import view.admin.DetailsPage;
 import view.admin.conn;
 import view.user.DetailsUserPage;
@@ -27,6 +28,7 @@ public class LoginPage extends Application {
     private TextField emailField;
     private PasswordField senhaField;
     private Button loginButton, cancelButton;
+    private Hyperlink registerLink;
 
     public static void main(String[] args) throws Exception{
         launch(args);
@@ -40,16 +42,20 @@ public class LoginPage extends Application {
         GridPane gridPane = createLoginForm();
         loginButton = createButton("Login", "#1E488F", "white");
         cancelButton = createButton("Cancel", "#dc3545", "white");
+        registerLink = createRegisterLink();
 
         loginButton.setOnAction(e -> login());
         cancelButton.setOnAction(e -> stage.close());
-
-        HBox buttonBox = new HBox(10, loginButton, cancelButton);
+        registerLink.setOnAction(e -> register());
+        VBox buttonBox = new VBox(10, loginButton, cancelButton);
         buttonBox.setAlignment(Pos.CENTER);
+
+        VBox hyperlinkBox = new VBox(registerLink);
+        hyperlinkBox.setAlignment(Pos.CENTER);
 
         VBox vbox = new VBox(20);
         vbox.setAlignment(Pos.CENTER);
-        vbox.getChildren().addAll(gridPane, buttonBox);
+        vbox.getChildren().addAll(gridPane, buttonBox, hyperlinkBox);
 
         Scene scene = new Scene(vbox, 600, 300);
         primaryStage.setScene(scene);
@@ -98,31 +104,43 @@ public class LoginPage extends Application {
             ResultSet result = c1.st.executeQuery(query);
             if (result.next()) {
                 int acesso = result.getInt("acesso");
-
                 if (acesso == 2) {                    
                     new DetailsPage().start(new Stage());
                     stage.close();
                 } else {
                     int id = result.getInt("id");                    
-                    String nome = result.getString("nome");
-                    String titular = result.getString("email");
-                    String tipoConta = result.getString("senha");
-                    int numConta = result.getInt("acesso");
+                    String userName = result.getString("nome");
+                    String userEmail = result.getString("email");
+                    String userAccountType = result.getString("senha");
+                    int accountNum = result.getInt("acesso");
 
-                    Usuario usuario = new Usuario(id, nome, titular, tipoConta, numConta);
-                    AppSession.setUsuarioLogado(usuario); 
+                    Usuario user = new Usuario(id, userName, userEmail, userAccountType, accountNum);
+                    AppSession.setUsuarioLogado(user); 
 
                     new DetailsUserPage().start(new Stage());
                     stage.close();
                 }
             } else {
-                AlertUtil.showErrorAlert(stage, "Login inválido!"); 
-                Stage viewWelcomeStage = new Stage();
-                WelcomePage viewEmployeePage = new WelcomePage();
-                viewEmployeePage.start(viewWelcomeStage);
+                AlertUtil.showErrorAlert(stage, "Login inválido!");
+                emailField.setDisable(false);
+                senhaField.setDisable(false);
+                cancelButton.setDisable(false);
             }
         } catch (Exception ex) {
             AlertUtil.showErrorAlert(stage,"Ocorreu um erro inesperado!");
         }
     }
+
+    private Hyperlink createRegisterLink() {
+        Hyperlink link = new Hyperlink("Ainda não possui conta? Se registrar");
+        link.setStyle( "-fx-text-fill:#1E488F ;");
+        return link;
+    }
+
+    private void register() {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+        new RegisterPage().start(new Stage());
+    } 
+    
 }
